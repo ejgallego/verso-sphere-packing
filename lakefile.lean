3dfc9532ec92f2dcea0b29f4d0ecdc1a9aa814b5
Emilio Jesus Gallego Arias <emilio@lean-fro.org>
2026-03-24 19:40:23 +0100

    chore(deps): bump VersoBlueprint to 0c236007b9e7

--- lakefile.lean
+++ lakefile.lean
@@ -2,8 +2,7 @@ import Lake
 open Lake DSL
 
 require verso from git "https://github.com/leanprover/verso"@"main"
-require VersoBlueprint from git "https://github.com/ejgallego/verso-blueprint"@"main"
-
+require VersoBlueprint from git "https://github.com/ejgallego/verso-blueprint"@"0c236007b9e702a188af84a2dc4f3725e778e2af"
 package VersoSpherePacking where
   precompileModules := false
   leanOptions := #[⟨`experimental.module, true⟩]
